8bfff146d6c7989f54ae7190083e27d3398a66d0
Usman Ali <60957279+uakk101@users.noreply.github.com>
2023-07-05 10:16:47 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Task from "./Task";
+import TaskList from "./TaskList";
+
+function App() {
+  return (
+    <div className="App">
+      <Task />
+      <TaskList />
+    </div>
+  );
+}
+
+export default App;
